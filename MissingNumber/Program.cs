6184106ad5d3b0949f9f0172293e52ec8d6bdcd7
Abraham Laria <abraham.laria@pnkmail.com>
2018-02-22 10:04:05 -0800

    Added some extra files to the repository

---
+++ MissingNumber/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MissingNumber
+{
+    class Program
+    {
+        public static int FindMissing(int[] array, int n)
+        {
+            var total = n*(n+1)/2; // 1+2+3+ ... +n = n*(n+1) this can be proved by math induction.
+
+            for (int i = 0; i < n-1; i++)
+            {
+                total -= array[i];
+            }
+
+            return total;
+        }
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 2, 4, 5, 6 };
+            Console.Write(FindMissing(array, 5));
+            Console.ReadLine();
+        }
+    }
+}
